8d6578a7aedd2461e717606e04eecdc37cbd6c81
Matayay Karuna <matayay2@gmail.com>
2023-03-08 02:37:33 -0700

    fixed bug in tic tac toe game

--- game.js
+++ game.js
@@ -5,42 +5,42 @@ const game = () => {
     {
         let game_end = true;
 
-        if ( board[0] == board[1] && board[0] == board[2] )
+        if ( board[0] == board[1] && board[0] == board[2] && board[0] != "N" )
         {
             return board[0];
         }
 
-        else if ( board[3] == board[4] && board[3] == board[5] )
+        else if ( board[3] == board[4] && board[3] == board[5] && board[3] != "N" )
         {
             return board[3];
         }
 
-        else if ( board[6] == board[7] && board[6] == board[8] )
+        else if ( board[6] == board[7] && board[6] == board[8] && board[6] != "N" )
         {
             return board[6];
         }
 
-        else if ( board[0] == board[3] && board[0] == board[6] )
+        else if ( board[0] == board[3] && board[0] == board[6] && board[0] != "N" )
         {
             return board[0];
         }
 
-        else if ( board[1] == board[4] && board[1] == board[7] )
+        else if ( board[1] == board[4] && board[1] == board[7] && board[1] != "N" )
         {
             return board[1];
         }
 
-        else if ( board[2] == board[5] && board[2] == board[8] )
+        else if ( board[2] == board[5] && board[2] == board[8] && board[2] != "N" )
         {
             return board[2];
         }
 
-        else if ( board[0] == board[4] && board[0] == board[8] )
+        else if ( board[0] == board[4] && board[0] == board[8] && board[0] != "N" )
         {
             return board[0];
         }
 
-        else if ( board[2] == board[4] && board[2] == board[6] )
+        else if ( board[2] == board[4] && board[2] == board[6] && board[2] != "N" )
         {
             return board[2];
         }
@@ -52,11 +52,12 @@ const game = () => {
                 if (board[i] == "N")
                 {
                     game_end = false;
+                    break;
                 }
             }
-
-            return game_end;
         }
+
+        return game_end;
     };
 
     const displayBoard = (game_board) => {
@@ -85,85 +86,62 @@ const game = () => {
 
     const playGame = () => {
         const squares = document.querySelectorAll('.square');
+        const web_board = document.querySelector('.board')
         const title = document.querySelector('.title');
         let red_turn = true;
-        let game_check;
+        let game_check = "NULL";
 
         for ( let i = 0; i < 9; i++ )
         {
-            if ( squares[i].textContent == "" )
-            {
-                squares[i].addEventListener('click', () => {
-                    if ( red_turn && board[i] == "N" )
-                    {
-                        board[i] = "X";
-                        red_turn = false;
-                        game_check = checkWin(board);
+            squares[i].addEventListener('click', () => {
+                if ( red_turn && board[i] == "N" )
+                {
+                    board[i] = "X";
+                    red_turn = false;
+                    displayBoard(board);
+                }
 
-                        if ( game_check == "X" || game_check == "O" )
-                        {
-                            for ( let i = 0; i < 9; i++ )
-                            {
-                                board[i] = "N";
-                            }
-
-                            title.innerHTML = `${game_check} Won`;
-                        }
-
-                        else if ( game_check == true )
-                        {
-                            for ( let i = 0; i < 9; i++ )
-                            {
-                                board[i] = "N";
-                            }
-
-                            title.innerHTML = `Tie`;
-                        }
-
-                        else
-                        {
-                            title.innerHTML = ``;
-                        }
-
-                        displayBoard(board);
-                    }
-
-                    else if ( !red_turn && board[i] == "N" )
-                    {
-                        board[i] = "O";
-                        red_turn = true;
-                        game_check = checkWin(board);
-
-                        if ( game_check == "X" || game_check == "O" )
-                        {
-                            for ( let i = 0; i < 9; i++ )
-                            {
-                                board[i] = "N";
-                            }
-
-                            title.innerHTML = `${game_check} Won`;
-                        }
-
-                        else if ( game_check == true )
-                        {
-                            for ( let i = 0; i < 9; i++ )
-                            {
-                                board[i] = "N";
-                            }
-
-                            title.innerHTML = `Tie`;
-                        }
-
-                        else
-                        {
-                            title.innerHTML = ``;
-                        }
-
-                        displayBoard(board);
-                    }
-                });
-            }
+                else if ( !red_turn && board[i] == "N" )
+                {
+                    board[i] = "O";
+                    red_turn = true;
+                    displayBoard(board);
+                }
+            });
         }
+
+        web_board.addEventListener('click', () => {
+            game_check = checkWin(board);
+
+            console.log(game_check);
+
+            if ( game_check == "X" || game_check == "O" )
+            {
+                for ( let i = 0; i < 9; i++ )
+                {
+                    board[i] = "N";
+                }
+
+                title.innerHTML = `${game_check} Won`;
+            }
+
+            else if ( game_check == true )
+            {
+                for ( let i = 0; i < 9; i++ )
+                {
+                    board[i] = "N";
+                }
+
+                title.innerHTML = `Tie`;
+            }
+
+            else
+            {
+                title.innerHTML = ``;
+            }
+
+            displayBoard(board);
+        });
     };
 
     return { playGame };
